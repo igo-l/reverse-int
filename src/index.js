f44c10b25d656c61734a0ca608a11d0753e758d8
igo-l <igolllf@gmail.com>
2021-08-12 12:22:43 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let reversNum = 0;
+  n=Math.abs(n);
+  while (n){
+    reversNum = reversNum * 10 + n % 10;
+    n = Math.floor(n / 10);
+  }
+  return reversNum;
 }
